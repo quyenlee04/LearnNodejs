9b581b14f14eb518f479ce842e15272c5e62f045
quyenlee04 <quyenle5184@gmail.com>
2025-01-10 14:47:56 +0700

    uploadd

--- README.md
+++ README.md
@@ -2,4 +2,4 @@ npm init
 npm install express
 npm install --save-dev nodemon
 Mở terminal gõ lệnh npm start để chạy node server
-express file upload: npm install --save multer
+express file upload: npm install multer --save
--- jadeEx/app.js
+++ jadeEx/app.js
@@ -1,7 +1,8 @@
 const bodyParser = require('body-parser');
 const { time } = require('console');
 const express = require('express'); 
-const path = require('path'); 
+const path = require('path');
+const multer = require('multer');
 const { title } = require('process');
 
 const app = express(); 
@@ -39,9 +40,37 @@ app.get('/register', (req, res) => {
      
 app.get('/upload', (req, res) => { 
         res.render(`upload`) ;
-     }); 
+     });
+
+// Cấu hình lưu trữ với multer để giữ nguyên tên file
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'uploads/'); // Thư mục lưu file
+    },
+    filename: (req, file, cb) => {
+        // Generate random filename with original extension
+        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+        const fileExtension = path.extname(file.originalname);
+        cb(null, uniqueSuffix + fileExtension);
+    },
+});
+// Khởi tạo multer với cấu hình storage
+const upload = multer({ storage: storage });
+
+app.post('/upload', upload.single('file'), (req, res) => {
+    const {username, password} = req.body;
+    const file = req.file;
 
 
+    res.send(
+        `<h1>Upload successful</h1>
+        <p>Username: ${username}</p>
+        <p>Password: ${password}</p>
+        <p>save: ${file.path}</p>
+        <p>File: ${file.originalname}</p>`
+
+    );
+});
 
 app.post('/user', (req, res) => {
 console.log(req.query);
